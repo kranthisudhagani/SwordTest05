39d0f12481e2da2536943423241c83d26b821f14
kranthi sudhagani <kranthikk999@gmail.com>
2021-01-24 19:28:57 +0000

    unit test

---
+++ MyUniteTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace MyUniteTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
